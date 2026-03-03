8fe9a85bc93d1dc27d95ae02c1b83a7cdef5b6df
tairen0319 <tairen.0319.kane@gmail.com>
2026-03-04 08:04:47 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBapimiG-dxZjyftV5kN9nIVFWVUFcjTHk",
+  authDomain: "iphone-resell-dc263.firebaseapp.com",
+  projectId: "iphone-resell-dc263",
+  storageBucket: "iphone-resell-dc263.firebasestorage.app",
+  messagingSenderId: "993125357871",
+  appId: "1:993125357871:web:782bc724a9ba7f291469da"
+};
